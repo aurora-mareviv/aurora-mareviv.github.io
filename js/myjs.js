c280cde9b4118088ea400fb01389a6e41e5e467d
aurora-mareviv <mariauror@gmail.com>
2017-08-20 22:46:26 +0200

    Start Hugo Blog

---
+++ js/myjs.js
@@ -0,0 +1,9 @@
+// silly js demo
+
+function alertColor() {
+  var result = "alert-info";
+  document.getElementById("suma").innerHTML = result; 
+  document.getElementById("myButton").style.backgroundColor = "#d9edf7";
+  document.getElementById("myButton").style.color = "#3a87ad";
+  document.getElementById("myButton").style.borderColor = "#bce8f1";
+}
