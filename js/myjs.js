b065c8f75dd73c5f3f3808ec07b8964b27d32894
aurora-mareviv <mariauror@gmail.com>
2017-08-22 16:16:42 +0200

    Start Hugo Blog

--- js/myjs.js
+++ js/myjs.js
@@ -1,8 +1,8 @@
 // silly js demo
 
 function alertColor() {
-  var result = "alert-info";
-  document.getElementById("suma").innerHTML = result; 
+  var result = "<span class='fa fa-info-circle'></span>&nbsp;alert-info";
+  document.getElementById("toInfo").innerHTML = result; 
   document.getElementById("myButton").style.backgroundColor = "#d9edf7";
   document.getElementById("myButton").style.color = "#3a87ad";
   document.getElementById("myButton").style.borderColor = "#bce8f1";
